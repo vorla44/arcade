7ef24eff3a541b10ef2fc294fd503031b291ecf5
Veikko Rintala <veikko0912@gmail.com>
2024-12-27 15:26:29 +0200

    random

--- funktio_kontrolli.py
+++ funktio_kontrolli.py
@@ -1,4 +1,8 @@
 import random
-my_number = random.random() * 5 + 10
-print(f"Satunnaisluku 10-15 väliltä {my_number}")
-
+quit = "n"
+while quit == "n":
+    print(f"Ohjelma jatkuu niin kauan, kuin vastaat {quit}")
+    my_number = random.random() * 5 + 10
+    print(f"Satunnaisluku 10-15 väliltä {my_number}")
+    quit = input("Do you want to quit? ")
+print(f"Silmukan suoritus loppui, kun vastasit {quit}")
